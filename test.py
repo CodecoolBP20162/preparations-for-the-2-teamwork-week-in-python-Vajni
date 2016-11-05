9b356e62eb79828fe8424f953471ebc19d96f3a2
Vajni <vajnork.zsolt@gmail.com>
2016-11-05 11:38:08 +0100

    Fixet isOdd testunit
    Ran the test 3 times, all the times returned ok.

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import unittest
 
-# Here's our "unit".
-
 
 def IsOdd(n):
-    return
+    
+    if n % 2 == 0:
+        return False
+    else:
+        return True
 
 # Here's our "unit tests".
 
@@ -12,10 +14,10 @@ def IsOdd(n):
 class IsOddTests(unittest.TestCase):
 
     def testOne(self):
-        self.failUnless(IsOdd(1))
+        self.assertTrue(IsOdd(1))
 
     def testTwo(self):
-        self.failIf(IsOdd(2))
+        self.assertFalse(IsOdd(2))
 
 
 def main():
